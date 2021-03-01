deac4e7fe4dd3ddc15ffa6b0155741841e52a5b9
andres julian barrera diaz <ing.julian.26@gmail.com>
2021-03-01 18:22:03 -0500

    Objetos_Clases_25_02_2021

---
+++ clases.js
@@ -0,0 +1,35 @@
+// class Animal{
+//     constructor(raz,esp,col){
+//         this.raza = raz;
+//         this.especie = esp;
+//         this.color = col;
+//     }
+//     set setraza(p1){
+//         this.raza = p1;
+//     }
+//     get getraza(){
+//         return this.raza;
+//     }
+//     set setespecie(p1){
+//         this.especie = p1;
+//     }
+//     get getespecie(){
+//         return this.especie;
+//     }
+//     set setcolor(p1){
+//         this.color = p1;
+//     }
+//     get getcolor(){
+//         return this.color;
+//     }
+//     ver_animal() {
+//         return `Animal: ${this.getespecie}, Raza: ${this.getraza}, Color : ${this.getcolor}`;
+//     }
+// }
+// console.log("clase \n");
+// const obj = new Animal("Angora", "Gato", "Negro");
+// console.log(obj.ver_animal());
+// obj.setraza = "Pincher";
+// obj.setespecie= "Perro";
+// obj.setcolor = "Blanco";
+// console.log(obj.ver_animal());
